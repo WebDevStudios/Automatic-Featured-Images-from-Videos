bfd5d4a25ba520500766c9f2905e6ae04234040c
Michael Beckwith <tw2113@gmail.com>
2016-07-06 01:22:31 -0500

    comment/phpdoc cleanup

--- automatic-featured-images-from-videos.php
+++ automatic-featured-images-from-videos.php
@@ -24,7 +24,12 @@ License: GPLv2
 */
 
 /**
- * If a YouTube or Vimeo video is added in the post content, grab its thumbnail and set it as the featured image
+ * If a YouTube or Vimeo video is added in the post content, grab its thumbnail and set it as the featured image.
+ *
+ * @since 1.0.0
+ *
+ * @param int    $post_id ID of the post being saved.
+ * @param object $post Post object.
  */
 function wds_set_media_as_featured_image( $post_id, $post ) {
 
@@ -36,15 +41,15 @@ function wds_set_media_as_featured_image( $post_id, $post ) {
 	// Only check the first 1000 characters of our post.
 	$content = substr( $content, 0, 800 );
 
-	// allow developers to filter the content to allow for searching in postmeta or other places
+	// Allow developers to filter the content to allow for searching in postmeta or other places.
 	$content = apply_filters( 'wds_featured_images_from_video_filter_content', $content );
 
-	// Props to @rzen for lending his massive brain smarts to help with the regex
+	// Props to @rzen for lending his massive brain smarts to help with the regex.
 	$do_video_thumbnail = (
 		$post_id
 		&& ! has_post_thumbnail( $post_id )
 		&& $content
-		// Get the video and thumb URLs if they exist
+		// Get the video and thumb URLs if they exist.
 		&& ( preg_match( '/\/\/(www\.)?(youtu|youtube)\.(com|be)\/(watch|embed)?\/?(\?v=)?([a-zA-Z0-9\-\_]+)/', $content, $youtube_matches )
 			|| preg_match( '#https?://(.+\.)?vimeo\.com/.*#i', $content, $vimeo_matches ) )
 	);
@@ -59,7 +64,7 @@ function wds_set_media_as_featured_image( $post_id, $post ) {
 	$vimeo_id = ! empty( $vimeo_matches ) ? preg_replace( "/[^0-9]/", "", $vimeo_matches[0] ) : '';
 
 	if ( $youtube_id ) {
-		// Check to see if our max-res image exists
+		// Check to see if our max-res image exists.
 		$file_headers = get_headers( 'http://img.youtube.com/vi/' . $youtube_id . '/maxresdefault.jpg' );
 		$is_404 = $file_headers[0] == 'HTTP/1.0 404 Not Found' || false !== strpos( $file_headers[0], '404 Not Found' );
 		$video_thumbnail_url = $is_404 ? 'http://img.youtube.com/vi/' . $youtube_id . '/maxresdefault.jpg' : 'http://img.youtube.com/vi/' . $youtube_id . '/hqdefault.jpg';
@@ -76,23 +81,33 @@ function wds_set_media_as_featured_image( $post_id, $post ) {
 
 	// If we found an image...
 	$attachment_id = $video_thumbnail_url && ! is_wp_error( $video_thumbnail_url )
-		// Then sideload it
+		// Then sideload it.
 		? wds_ms_media_sideload_image_with_new_filename( $video_thumbnail_url, $post_id, sanitize_title( preg_replace( "/[^a-zA-Z0-9\s]/", "-", get_the_title() ) ) )
-		// No thumbnail url found
+		// No thumbnail url found.
 		: 0;
 
-	// If attachment wasn't created, bail
+	// If attachment wasn't created, bail.
 	if ( ! $attachment_id ) {
 		return;
 	}
 
-	// Woot! we got an image, so set it as the post thumbnail
+	// Woot! we got an image, so set it as the post thumbnail.
 	set_post_thumbnail( $post_id, $attachment_id );
 	update_post_meta( $post_id, '_is_video', true );
 
 }
 add_action( 'save_post', 'wds_set_media_as_featured_image', 10, 2 );
 
+/**
+ * Handle the upload of a new image.
+ *
+ * @since 1.0.0
+ *
+ * @param string      $url      URL to sideload.
+ * @param int         $post_id  Post ID to attach to.
+ * @param string|null $filename Filename to use.
+ * @return mixed
+ */
 function wds_ms_media_sideload_image_with_new_filename( $url, $post_id, $filename = null ) {
 
 	if ( ! $url || ! $post_id ) {
@@ -101,73 +116,72 @@ function wds_ms_media_sideload_image_with_new_filename( $url, $post_id, $filenam
 
 	require_once( ABSPATH . 'wp-admin/includes/file.php' );
 
-	// Download file to temp location, returns full server path to temp file, ex; /home/user/public_html/mysite/wp-content/26192277_640.tmp
+	// Download file to temp location, returns full server path to temp file, ex; /home/user/public_html/mysite/wp-content/26192277_640.tmp.
 	$tmp = download_url( $url );
 
-	// If error storing temporarily, unlink
+	// If error storing temporarily, unlink.
 	if ( is_wp_error( $tmp ) ) {
-		// clean up
+		// Clean up.
 		@unlink( $file_array['tmp_name'] );
 		$file_array['tmp_name'] = '';
-		// and output wp_error
+		// And output wp_error.
 		return $tmp;
 	}
 
-	// fix file filename for query strings
+	// Fix file filename for query strings.
 	preg_match( '/[^\?]+\.(jpg|JPG|jpe|JPE|jpeg|JPEG|gif|GIF|png|PNG)/', $url, $matches );
-	// extract filename from url for title
+	// Extract filename from url for title.
 	$url_filename = basename($matches[0]);
-	// determine file type (ext and mime/type)
+	// Determine file type (ext and mime/type).
 	$url_type = wp_check_filetype($url_filename);
 
-	// override filename if given, reconstruct server path
+	// Override filename if given, reconstruct server path.
 	if ( !empty( $filename ) ) {
 		$filename = sanitize_file_name( $filename );
-		// extract path parts
+		// Extract path parts.
 		$tmppath = pathinfo( $tmp );
-		// build new path
+		// Build new path.
 		$new = $tmppath['dirname'] . '/'. $filename . '.' . $tmppath['extension'];
-		// renames temp file on server
+		// Renames temp file on server.
 		rename($tmp, $new);
-		// push new filename (in path) to be used in file array later
+		// Push new filename (in path) to be used in file array later.
 		$tmp = $new;
 	}
 
-	// assemble file data (should be built like $_FILES since wp_handle_sideload() will be using)
+	/* Assemble file data (should be built like $_FILES since wp_handle_sideload() will be using). */
 
-	// full server path to temp file
+	// Full server path to temp file.
 	$file_array['tmp_name'] = $tmp;
 
 	if ( !empty( $filename ) ) {
-		// user given filename for title, add original URL extension
+		// User given filename for title, add original URL extension.
 		$file_array['name'] = $filename . '.' . $url_type['ext'];
 	} else {
-		// just use original URL filename
+		// Just use original URL filename.
 		$file_array['name'] = $url_filename;
 	}
 
 	$post_data = array(
-		// just use the original filename (no extension)
+		// Just use the original filename (no extension).
 		'post_title' => get_the_title( $post_id ),
-		// make sure gets tied to parent
+		// Make sure gets tied to parent.
 		'post_parent' => $post_id,
 	);
 
-	// required libraries for media_handle_sideload
+	// Required libraries for media_handle_sideload.
 	require_once( ABSPATH . 'wp-admin/includes/file.php' );
 	require_once( ABSPATH . 'wp-admin/includes/media.php' );
 	require_once( ABSPATH . 'wp-admin/includes/image.php' );
 
-	// do the validation and storage stuff
-
-	// $post_data can override the items saved to wp_posts table, like post_mime_type, guid, post_parent, post_title, post_content, post_status
+	// Do the validation and storage stuff.
+	// $post_data can override the items saved to wp_posts table, like post_mime_type, guid, post_parent, post_title, post_content, post_status.
 	$att_id = media_handle_sideload( $file_array, $post_id, null, $post_data );
 
-	// If error storing permanently, unlink
+	// If error storing permanently, unlink.
 	if ( is_wp_error( $att_id ) ) {
-		// clean up
+		// Clean up.
 		@unlink( $file_array['tmp_name'] );
-		// and output wp_error
+		// And output wp_error.
 		return $att_id;
 	}
 
