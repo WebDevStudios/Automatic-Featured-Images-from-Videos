d409c62ce9fbbdb70776e2f64d21dc204896be0d
Gary Kovar <gkovar@gmail.com>
2016-11-17 21:47:21 -0500

    Added video_embed_url to vimeo return.

--- automatic-featured-images-from-videos.php
+++ automatic-featured-images-from-videos.php
@@ -76,7 +76,7 @@ function wds_check_if_content_contains_video( $post_id, $post ) {
 		$vimeo_details       = wds_get_vimeo_details( $vimeo_id );
 		$video_thumbnail_url = $vimeo_details['video_thumbnail_url'];
 		$video_url           = $vimeo_details['video_url'];
-		$video_embed_url     = $vimeo_details['video_url'];
+		$video_embed_url     = $vimeo_details['video_embed_url'];
 	}
 
 	if ( $post_id
@@ -295,6 +295,7 @@ function wds_get_vimeo_details( $vimeo_id ) {
 		$response                     = unserialize( $vimeo_data['body'] );
 		$video['video_thumbnail_url'] = isset( $response[0]['thumbnail_large'] ) ? $response[0]['thumbnail_large'] : false;
 		$video['video_url']           = 'https://vimeo.com/' . $vimeo_id;
+		$video['video_embed_url']     = 'player.vimeo.com/video/' . $vimeo_id;
 	}
 
 	return $video;
