a5260da047782e310558754f2a78b413f44ab057
Gary Kovar <gkovar@gmail.com>
2016-11-19 21:50:36 -0500

    Trying to check the scheduled events.

--- automatic-featured-images-from-videos.php
+++ automatic-featured-images-from-videos.php
@@ -364,24 +364,30 @@ function wds_customize_post_buttons() {
 	$type_array = array( 'post', 'page' );
 
 	if ( in_array( $post_type, $type_array ) ) {
-		// @TODO This can all be displayed conditionally based on if an action is running or not.
-
-		?>
-		<script>
-			jQuery( function () {
-				jQuery( "body.post-type-<?php echo $post_type; ?> .wrap h1" ).append( '<a href="#" class="page-title-action bulk-add-video">Bulk add Video Thumbnails</a>' );
-				jQuery( ".bulk-add-video" ).click( function () {
-					jQuery( ".bulk-add-video" ).hide();
-					jQuery( "body.post-type-<?php echo $post_type; ?> .wrap h1" ).append( '<a class="page-title-action bulk-add-video-status">Processing...</a>' );
-					jQuery.ajax( {
-						type: "POST",
-						url:  ajaxurl,
-						data: { action: 'wds_queue_bulk_processing', posttype: '<?php echo $post_type; ?>' }
+		if ( ! wp_next_scheduled('wds_bulk_process_video_query_init') ) {
+			?>
+			<script>
+				jQuery( function () {
+					jQuery( "body.post-type-<?php echo $post_type; ?> .wrap h1" ).append( '<a href="#" class="page-title-action bulk-add-video">Bulk add Video Thumbnails</a>' );
+					jQuery( ".bulk-add-video" ).click( function () {
+						jQuery( ".bulk-add-video" ).hide();
+						jQuery( "body.post-type-<?php echo $post_type; ?> .wrap h1" ).append( '<a class="page-title-action bulk-add-video-status">Processing...</a>' );
+						jQuery.ajax( {
+							type: "POST",
+							url:  ajaxurl,
+							data: { action: 'wds_queue_bulk_processing', posttype: '<?php echo $post_type; ?>' }
+						} );
 					} );
 				} );
-			} );
-		</script>
-		<?php
+			</script>
+			<?php } else { ?>
+			<script>
+				jQuery( function () {
+					jQuery( "body.post-type-<?php echo $post_type; ?> .wrap h1" ).append( '<a class="page-title-action bulk-add-video-status">Processing...</a>' );
+				} );
+			</script>
+			<?php
+		}
 	}
 }
 
