c04fc0f228b746682576d4105c5fc163d6714c78
Gary Kovar <gkovar@gmail.com>
2016-11-28 20:55:20 -0500

    Internationalized the button text.

--- includes/ajax.php
+++ includes/ajax.php
@@ -27,10 +27,11 @@ function wds_customize_post_buttons() {
 	$type_array = apply_filters( 'wds_featured_images_from_video_post_types', $type_array );
 
 	if ( in_array( $post_type, $type_array ) ) {
-
 		$args = array(
-			'post_type' => $post_type,
-			'status'    => wds_featured_images_from_video_processing_status( $post_type ),
+			'post_type'       => $post_type,
+			'status'          => wds_featured_images_from_video_processing_status( $post_type ),
+			'processing_text' => __( 'Processing...', 'wds_automatic_featured_images_from_videos' ),
+			'bulk_text'       => __( 'Bulk add Video Thumbnails', 'wds_automatic_featured_images_from_videos' ),
 		);
 
 		wp_localize_script( 'wds_featured_images_from_video_script', 'wds_featured_image_from_vid_args', $args );
--- js/button.js
+++ js/button.js
@@ -2,19 +2,17 @@
  * Created by garykovar on 11/21/16.
  */
 
-console.log('hello');
-
 jQuery( document ).ready(function(){
 
 	if ('running' == wds_featured_image_from_vid_args.status ) {
-		jQuery( "body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1" ).append( '<a class="page-title-action bulk-add-video-status">Processing...</a>' );
+		jQuery( "body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1" ).append( '<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>' );
 	}
 
 	if ('ready_to_process' == wds_featured_image_from_vid_args.status ) {
-		jQuery( "body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1" ).append( '<a href="#" class="page-title-action bulk-add-video">Bulk add Video Thumbnails</a>' );
+		jQuery( "body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1" ).append( '<a href="#" class="page-title-action bulk-add-video">' + wds_featured_image_from_vid_args.bulk_text + '</a>' );
 		jQuery( ".bulk-add-video" ).click( function () {
 			jQuery( ".bulk-add-video" ).hide();
-			jQuery( "body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1" ).append( '<a class="page-title-action bulk-add-video-status">Processing...</a>' );
+			jQuery( "body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1" ).append( '<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>' );
 			jQuery.ajax( {
 				type: "POST",
 				url:  ajaxurl,
