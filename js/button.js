4760a18650b2977d8ae28be6eab8fd4344fd8593
Gary Kovar <gkovar@gmail.com>
2017-02-25 21:50:19 -0500

    Fixes #41 (character count).

--- js/button.js
+++ js/button.js
@@ -2,13 +2,13 @@
  * Created by garykovar on 11/21/16.
  */
 
-jQuery( document ).ready(function(){
+jQuery( document ).ready( function () {
 
-	if ('running' == wds_featured_image_from_vid_args.status ) {
+	if ( 'running' == wds_featured_image_from_vid_args.status ) {
 		jQuery( "body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1" ).append( '<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>' );
 	}
 
-	if ('ready_to_process' == wds_featured_image_from_vid_args.status ) {
+	if ( 'ready_to_process' == wds_featured_image_from_vid_args.status ) {
 		jQuery( "body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1" ).append( '<a href="#" class="page-title-action bulk-add-video">' + wds_featured_image_from_vid_args.bulk_text + '</a>' );
 		jQuery( ".bulk-add-video" ).click( function () {
 			jQuery( ".bulk-add-video" ).hide();
@@ -18,6 +18,6 @@ jQuery( document ).ready(function(){
 				url:  ajaxurl,
 				data: { action: 'wds_queue_bulk_processing', posttype: wds_featured_image_from_vid_args.post_type }
 			} );
-		});
+		} );
 	}
-});
+} );
