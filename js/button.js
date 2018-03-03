6e804e5e9ffba0ae01d1c25709365caf061e433e
Michael Beckwith <tw2113@gmail.com>
2018-03-03 15:19:29 -0600

    more details from branches

--- js/button.js
+++ js/button.js
@@ -4,21 +4,25 @@
 
 jQuery(document).ready(function () {
 
-	if ('running' == wds_featured_image_from_vid_args.status) {
-		jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>');
-	}
-
-	if ('ready_to_process' == wds_featured_image_from_vid_args.status) {
+	function wds_ajax_maybe_update_post_thumbnail() {
 		jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a href="#" class="page-title-action bulk-add-video">' + wds_featured_image_from_vid_args.bulk_text + '</a>');
 		jQuery(".bulk-add-video").on('click',function (e) {
 			e.preventDefault();
 			jQuery(".bulk-add-video").hide();
-			jQuery("body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>');
+			jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>');
 			jQuery.ajax({
 				type: "POST",
 				url : ajaxurl,
 				data: {action: 'wds_queue_bulk_processing', posttype: wds_featured_image_from_vid_args.post_type}
 			});
 		});
+	}
+
+	if ('running' == wds_featured_image_from_vid_args.status) {
+		jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>');
+	}
+
+	if ('ready_to_process' == wds_featured_image_from_vid_args.status) {
+		wds_ajax_maybe_update_post_thumbnail();
 	}
 });
