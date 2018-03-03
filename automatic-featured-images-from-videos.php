5ff19ae16d32ee8dcc3e5634b23b9e21ddf008b5
Michael Beckwith <tw2113@gmail.com>
2018-03-03 15:03:45 -0600

    reformat and rework for easier video url updating

--- automatic-featured-images-from-videos.php
+++ automatic-featured-images-from-videos.php
@@ -350,11 +350,28 @@ function wds_ms_media_sideload_image_with_new_filename( $url, $post_id, $filenam
  */
 function wds_get_youtube_details( $youtube_id ) {
 	$video = array();
+	$video_thumbnail_url_string = 'http://img.youtube.com/vi/%s/%s';
 
-	$video                        = wp_remote_head( 'https://www.youtube.com/oembed?format=json&url=http://www.youtube.com/watch?v=' . $youtube_id );
-	if ( 200 === wp_remote_retrieve_response_code( $video ) ) {
-		$remote_headers               = wp_remote_head( 'http://img.youtube.com/vi/' . $youtube_id . '/maxresdefault.jpg' );
-		$video['video_thumbnail_url'] = ( 404 === wp_remote_retrieve_response_code( $remote_headers ) ) ? 'http://img.youtube.com/vi/' . $youtube_id . '/hqdefault.jpg' : 'http://img.youtube.com/vi/' . $youtube_id . '/maxresdefault.jpg';
+	$video_check                      = wp_remote_head( 'https://www.youtube.com/oembed?format=json&url=http://www.youtube.com/watch?v=' . $youtube_id );
+	if ( 200 === wp_remote_retrieve_response_code( $video_check ) ) {
+		$remote_headers               = wp_remote_head(
+			sprintf(
+				$video_thumbnail_url_string,
+				$youtube_id,
+				'/maxresdefault.jpg'
+			)
+		);
+		$video['video_thumbnail_url'] = ( 404 === wp_remote_retrieve_response_code( $remote_headers ) ) ?
+			sprintf(
+				$video_thumbnail_url_string,
+				$youtube_id,
+				'/hqdefault.jpg'
+			) :
+			sprintf(
+				$video_thumbnail_url_string,
+				$youtube_id,
+				'/maxresdefault.jpg'
+			);
 		$video['video_url']           = 'https://www.youtube.com/watch?v=' . $youtube_id;
 		$video['video_embed_url']     = 'https://www.youtube.com/embed/' . $youtube_id;
 	}
