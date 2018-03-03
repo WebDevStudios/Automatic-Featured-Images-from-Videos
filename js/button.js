13a5698e08dedc885826927b0cf64f71e8ca98a4
Michael Beckwith <tw2113@gmail.com>
2018-03-03 14:54:55 -0600

    prevent showing up on post editor screen

--- js/button.js
+++ js/button.js
@@ -5,11 +5,11 @@
 jQuery(document).ready(function () {
 
 	if ('running' == wds_featured_image_from_vid_args.status) {
-		jQuery("body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>');
+		jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>');
 	}
 
 	if ('ready_to_process' == wds_featured_image_from_vid_args.status) {
-		jQuery("body.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a href="#" class="page-title-action bulk-add-video">' + wds_featured_image_from_vid_args.bulk_text + '</a>');
+		jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a href="#" class="page-title-action bulk-add-video">' + wds_featured_image_from_vid_args.bulk_text + '</a>');
 		jQuery(".bulk-add-video").on('click',function (e) {
 			e.preventDefault();
 			jQuery(".bulk-add-video").hide();
