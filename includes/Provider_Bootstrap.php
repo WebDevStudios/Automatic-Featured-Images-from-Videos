3fddea8ec6435c9d51f73b85be414f1856b64162
Gary Kovar <gkovar@gmail.com>
2018-05-28 20:34:36 -0400

    removes povider filter in favor of fairly accessible action

--- includes/Provider_Bootstrap.php
+++ includes/Provider_Bootstrap.php
@@ -4,24 +4,20 @@ class Provider_Bootstrap {
 
 	protected $providers = [];
 
-	public function add_provider( $provider ) {
+	public function add_provider( $provider_name, $provider ) {
 		if ( ! is_a( $provider, Video_Provider::class ) ) {
 			return new WP_Error( __( 'The passed provider is not a Provider Object', 'automatic-featured-images-from-videos' ) );
 		}
 
-		$this->providers[] = $provider;
+		$this->providers[ $provider_name ] = $provider;
+	}
+
+	public function remove_provider( $provider_name ) {
+		unset( $this->providers[ $provider_name ] );
 	}
 
 	public function video_providers() {
-		/**
-		 * Allow developers to pass in custom video providers.
-		 * Video providers should extend the Video_Provider class.
-		 *
-		 * @since 1.1.2
-		 *
-		 * @param Video_Provider $value An object that is of a class that extends Video_Provider.
-		 */
-		return apply_filters( 'wds_video_providers', $this->providers );
+		return $this->providers;
 	}
 
 }
