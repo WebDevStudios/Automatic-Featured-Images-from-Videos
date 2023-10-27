bf0ef68f99ceace5f9146c1c578b182479e16917
Michael Beckwith <tw2113@gmail.com>
2023-10-27 08:36:55 -0500

    main php file version bump

--- automatic-featured-images-from-videos.php
+++ automatic-featured-images-from-videos.php
@@ -3,7 +3,7 @@
  * Plugin Name: Automatic Featured Images from YouTube / Vimeo
  * Plugin URI: https://webdevstudios.com
  * Description: If a YouTube or Vimeo video exists in the first few paragraphs of a post, automatically set the post's featured image to that video's thumbnail.
- * Version: 1.2.1
+ * Version: 1.2.2
  * Author: WebDevStudios
  * Author URI: https://webdevstudios.com
  * License: GPLv2
