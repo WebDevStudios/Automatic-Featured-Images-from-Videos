3446d2c5248c4b74b7d6a1b4d080406b81fc80f5
Michael Beckwith <tw2113@gmail.com>
2024-09-26 19:47:37 -0500

    needs testing/confirmation, but convert to vanilla js

--- js/button.js
+++ js/button.js
@@ -1,28 +1,46 @@
-/**
- * Created by garykovar on 11/21/16.
- */
+'use strict';
 
-jQuery(document).ready(function () {
+(() => {
+	const h1edit = document.querySelector('body.edit-php.post-type-' + wds_featured_image_from_vid_args.post_type + ' .wrap h1');
+
+	const processLink = document.createElement('a');
+	processLink.href = '#';
+	processLink.classList.add('page-title-action', 'bulk-add-video');
+	processLink.innerHTML = wds_featured_image_from_vid_args.bulk_text;
+
+	const runningLink = document.createElement('a');
+	runningLink.classList.add('page-title-action', 'bulk-add-video-status');
+	runningLink.innerHTML = wds_featured_image_from_vid_args.processing_text;
 
 	function wds_ajax_maybe_update_post_thumbnail() {
-		jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a href="#" class="page-title-action bulk-add-video">' + wds_featured_image_from_vid_args.bulk_text + '</a>');
-		jQuery(".bulk-add-video").on('click',function (e) {
+		h1edit.append(processLink);
+		processLink.addEventListener('click', (e) => {
 			e.preventDefault();
-			jQuery(".bulk-add-video").hide();
-			jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>');
-			jQuery.ajax({
-				type: "POST",
-				url : ajaxurl,
-				data: {action: 'wds_queue_bulk_processing', posttype: wds_featured_image_from_vid_args.post_type}
+			e.target.style.display = 'none';
+			h1edit.append(runningLink);
+
+			const data = new FormData();
+			data.append('action', 'wds_queue_bulk_processing');
+			data.append('posttype', wds_featured_image_from_vid_args.post_type);
+			const options = {
+				method: 'POST', body: data,
+			}
+
+			fetch(window.ajaxurl, options)
+				.then((response) => response.json())
+				.then((response) => {
+					console.log(response)
+				}).catch((error) => {
+				console.log(error);
 			});
 		});
 	}
 
-	if ('running' == wds_featured_image_from_vid_args.status) {
-		jQuery("body.edit-php.post-type-" + wds_featured_image_from_vid_args.post_type + " .wrap h1").append('<a class="page-title-action bulk-add-video-status">' + wds_featured_image_from_vid_args.processing_text + '</a>');
+	if ('running' === wds_featured_image_from_vid_args.status) {
+		h1edit.append(runningLink);
 	}
 
-	if ('ready_to_process' == wds_featured_image_from_vid_args.status) {
+	if ('ready_to_process' === wds_featured_image_from_vid_args.status) {
 		wds_ajax_maybe_update_post_thumbnail();
 	}
-});
+})();
